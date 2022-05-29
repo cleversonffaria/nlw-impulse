b2bef81a7f2b3f477d731088906d682688d850a7
cleversonffaria <cleversonffaria@gmail.com>
2022-05-29 14:50:05 -0300

    create frontend and backend

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "8408e764a794c6",
+    pass: "10bf63a38a9ff7",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Cléverson Fernandes <cleversonffaria@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
